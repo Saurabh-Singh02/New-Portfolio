4f7da1076db10b8daf61ba789eb05e9f0c5c18ac
Saurabh Singh <ssthakur.code@gmail.com>
2023-07-07 17:02:32 +0530

    sixth commit

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,11 +1,14 @@
 function hambtogg() {
     let x = document.getElementById("nav-content");
+    let y = document.getElementById("hamburger");
      {
          if(x.style.display ==="none")
          {
              x.style.display = "block";
-         }else {
-             x.style.display = "none";
+             y.style.backgroundColor = "#ff7300";
+            }else {
+                x.style.display = "none";
+                y.style.backgroundColor = "#ffffff";
          }
      }
    }
