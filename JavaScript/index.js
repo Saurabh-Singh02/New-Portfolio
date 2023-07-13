826eec71ff88fb45eb2455a66e6af93b3ed33650
Saurabh Singh <ssthakur.code@gmail.com>
2023-07-13 17:13:00 +0530

    thirteenth commit

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,21 +1,20 @@
 function hambtogg() {
     let x = document.getElementById("nav-content");
     let y = document.getElementById("hamburger");
-     {
-         if(x.style.display ==="none")
-         {
-             x.style.display = "block";
-             y.style.backgroundColor = "#177caf";
-            }else {
-                x.style.display = "none";
-                y.style.backgroundColor = "#ffffff";
-         }
-     }
-   }
-
-   function ThemeChange() {
+    
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      y.style.backgroundColor = "#177caf";
+    } else {
+      x.style.display = "none";
+      y.style.backgroundColor = "#000000";
+    }
+  }
+  
+  function ThemeChange() {
     var element = document.getElementById("body");
     element.classList.toggle("dark-theme");
     var elembutton = document.getElementById("themebtn");
     elembutton.classList.toggle("button-light");
-   }
+  }
+  
